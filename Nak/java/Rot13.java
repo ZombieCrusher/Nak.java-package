20031ba875ef8e57b3bdf99c108089dbfe577523
ZombieCrsuher <nakshatra.pyne@gmail.com>
2024-03-18 22:28:21 +0530

    Update Rot13.java
    Added a description for Rot13

--- Nak/java/Rot13.java
+++ Nak/java/Rot13.java
@@ -1,3 +1,14 @@
+/*
+ROT13 (Rotate13, "rotate by 13 places", sometimes hyphenated ROT-13) is a simple letter substitution cipher that replaces a letter with the 13th letter after it in the Latin alphabet. 
+ROT13 is a special case of the Caesar cipher which was developed in ancient Rome.
+Because there are 26 letters (2×13) in the basic Latin alphabet, ROT13 is its own inverse; 
+that is, to undo ROT13, the same algorithm is applied, so the same action can be used for encoding and decoding. 
+The algorithm provides virtually no cryptographic security, and is often cited as a canonical example of weak encryption.
+ROT13 was used in online forums as a means of hiding spoilers, punchlines, puzzle solutions, and offensive materials from the casual glance. 
+ROT13 has inspired a variety of letter and word games online, and is frequently mentioned in newsgroup conversations.
+*/
+
+
 package Nak.java;
 
 import java.lang.*;
@@ -7,7 +18,7 @@ public final class Rot13
 {
     private String OriginalMessage = "";
     private String EncryptedMessage = "";
-    
+
     public Rot13( String word , boolean isEncrypted )
     {
         if( isEncrypted )
