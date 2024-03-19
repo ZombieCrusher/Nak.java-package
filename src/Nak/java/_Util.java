3c2effa3348487f881ba8e544e61e31695757f3c
ZombieCrsuher <nakshatra.pyne@gmail.com>
2024-03-19 17:34:52 +0530

    Update _Util.java
    Added Supression line for Java 18+

--- src/Nak/java/_Util.java
+++ src/Nak/java/_Util.java
@@ -62,6 +62,7 @@ public final class _Util
         }
     }
 
+    @SuppressWarnings("deprecation")
     public static void shutdownWindows()
     {
         try 
@@ -77,6 +78,7 @@ public final class _Util
         }
     }
 
+    @SuppressWarnings("deprecation")
     public static void shutdownWindows( short time )
     {
         try
